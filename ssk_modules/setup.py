86943d9eec9e9bb171ff1d6f54fc245a1d587e32
Magda Radić <magdina.radic@gmail.com>
2023-12-15 16:53:44 +0100

    added ssk modules

---
+++ ssk_modules/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, Extension
+from Cython.Build import cythonize
+import numpy as np
+
+# Specify the include directories for NumPy
+include_dirs = [np.get_include()]
+
+# Define the Cython extension
+extensions = [
+    Extension("string_kernel", ["string_kernel.pyx"], include_dirs=include_dirs),
+]
+
+# Setup script
+setup(
+    ext_modules=cythonize(extensions),
+    include_dirs=[np.get_include()],  # This might not be necessary, but include it just in case
+)
